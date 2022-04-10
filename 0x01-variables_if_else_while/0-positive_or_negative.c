9634841ef6804fdf0271f0b34f34fc4b2317e479
Patrick NDAHAYO <ndatrick@gmail.com>
2022-04-10 05:33:40 -0700

    Positive or Negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,10 +13,10 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 printf("%d", n);
 if (n > 0)
-printf("%d is positive\n");
+printf(" is positive\n");
 else if (n == 0)
-printf("%d is zero\n");
+printf(" is zero\n");
 else
-printf("%d is negative\n");
+printf(" is negative\n");
 return (0);
 }
