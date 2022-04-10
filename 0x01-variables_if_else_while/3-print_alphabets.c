17ee691ec5c34db6f6ba9edad068a30cf91d3513
Patrick NDAHAYO <ndatrick@gmail.com>
2022-04-10 06:15:38 -0700

    Alphabet print again

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - function
+ *Return: 0
+ */
+int main(void)
+{
+char c = 'a';
+char d = 'A';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+while (d <= 'Z')
+{
+putchar(d);
+d++;
+}
+putchar('\n');
+return (0);
+}
