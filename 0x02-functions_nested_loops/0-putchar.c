3cf1a767bb40bb8badc2f1e32cfaf65ab50eb454
Patrick NDAHAYO <ndatrick@gmail.com>
2022-04-12 23:29:26 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#inlude "0-putchar.h"
+/**
+ * main - Entry point
+ * Return:0
+ */
+int main(void)
+{
+char c[10] = "0-putchar";
+int i = 0;
+while (i < 9)
+{
+_putchar(c[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
