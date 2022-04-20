6dc2491c0328b11d92d3d55c00d5423f5c4fb1f4
Patrick NDAHAYO <ndatrick@gmail.com>
2022-04-20 11:21:25 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -24,30 +24,4 @@ _putchar (str[i]);
 
 _putchar ('\n');
 
-}#include "main.h"
-/**
- * puts_half - prints a string, followed by a new line,
- * @str: pointer to the string to print
- * Return: void
-*/
-void puts_half(char *str)
-
-{
-int i, last;
-
-i = 0;
-while (str[i] != '\0')
-{
-i++;
-}
-
-last = (i + 1) / 2;
-
-for (i = last; str[i]; i++)
-{
-_putchar (str[i]);
-}
-
-_putchar ('\n');
-
 }
